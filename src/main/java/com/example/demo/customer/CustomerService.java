0bafe967442c66ddd46d0bab6ca622448f33c30e
Martin <martinkabe@seznam.cz>
2021-03-28 21:03:28 +0200

    Add org.slf4j.Logger

--- src/main/java/com/example/demo/customer/CustomerService.java
+++ src/main/java/com/example/demo/customer/CustomerService.java
@@ -1,12 +1,16 @@
 package com.example.demo.customer;
 
 import com.example.demo.exception.NotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class CustomerService {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(CustomerService.class);
 
     private final CustomerRepository customerRepository;
 
@@ -15,11 +19,16 @@ public class CustomerService {
     }
 
     public List<Customer> getCustomers() {
+        LOGGER.info("getCustomers was called");
         return customerRepository.findAll();
     }
 
     public Customer getCustomer(Long id) {
         return customerRepository.findById(id)
-                .orElseThrow(() -> new NotFoundException("Customer with id " + id + " doesn't exist"));
+                .orElseThrow(() -> {
+                    NotFoundException notFoundException = new NotFoundException("Customer with id " + id + " doesn't exist");
+                    LOGGER.error("error in getting customer {}", id, notFoundException);
+                    return notFoundException;
+                });
     }
 }
