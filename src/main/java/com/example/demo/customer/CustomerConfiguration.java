557d622892f6bd00ecb87f988c8d374af7bfdbf3
Martin <martinkabe@seznam.cz>
2021-04-03 19:51:42 +0200

    Reduce unused code

--- src/main/java/com/example/demo/customer/CustomerConfiguration.java
+++ src/main/java/com/example/demo/customer/CustomerConfiguration.java
@@ -1,7 +1,6 @@
 package com.example.demo.customer;
 
 import com.example.demo.infoapp.AppInfo;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
